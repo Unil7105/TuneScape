a5829b345ce453d7023293e430df4759553dc291
Unil Prajapati <prajapati.unil26@gmail.com>
2026-02-01 23:38:54 +0530

    fix

--- components/Visualizer.tsx
+++ components/Visualizer.tsx
@@ -66,7 +66,6 @@ const Visualizer: React.FC<VisualizerProps> = ({ isPlaying, audioRef }) => {
   return (
     <div className="w-full h-10 overflow-hidden flex items-center justify-center">
       <svg ref={svgRef} width="100" height="40" className="opacity-60" />
-
     </div>
   );
 };
